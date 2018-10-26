09273e7fe239a9cd07f2203f015a8910ac041383
Ryan Luu <rluu@purdue.edu>
2018-10-25 20:30:24 -0400

    Reset venv, added parser, added gitignore

--- corec.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import urlopen
-import json
-import pprint
-
-response = urlopen("https://www.purdue.edu/drsfacilityusage/api/CurrentActivity")
-data = json.loads(response.read().decode("utf-8"))
-
-pprint.PrettyPrinter(indent=4).pprint(data)
