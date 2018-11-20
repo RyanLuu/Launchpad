02dc090ed94fdd49091782a610a944a5cc378491
Ryan Luu <rluu@purdue.edu>
2018-11-19 19:35:22 -0500

    Graphing progress + OO

---
+++ cast.py
@@ -0,0 +1,65 @@
+import re
+
+def line_to_words(line):
+    return re.findall(r"\b[A-Za-z]+(?:[-'][A-Za-z]+)?\b", line)
+
+class Cast:
+
+    def __init__(self):
+        self.characters = []
+
+    def get_character(self, name):
+        return next((c for c in self.characters if c.name == name), None)
+
+    def __add__(self, other):
+        
+        if isinstance(other, str):
+            other = Character(other)
+
+        if isinstance(other, Character):
+            if not self.__contains__(other):
+                self.characters.append(other)
+            else:
+                print("we")
+            return self
+        else:
+            return NotImplementedError
+        
+    def __iter__(self):
+        return iter(self.characters)
+    
+    def __contains__(self, key):
+        if isinstance(key, Character):
+            return key.name in [c.name for c in self.characters]
+        
+        if isinstance(key, str):
+            return key in [c.name for c in self.characters]
+        return NotImplementedError
+
+    def __getitem__(self, key):
+        return self.characters[key]
+
+    def __str__(self):
+        return "{{{}}}".format(", ".join((str(c) for c in self.characters)))
+
+class Character:
+
+    def __init__(self, name):
+        self.name = name
+        self.lines = []
+        self.words = []
+
+    def add_line(self, line):
+        self.lines.append(line)
+        self.words.extend(line_to_words(line))
+
+    @property
+    def average_wpL(self):
+        return len(self.words) / len(self.lines)
+
+    @property
+    def average_lpw(self):
+        return len(''.join(self.words)) / len(self.words)
+
+    def __str__(self):
+        return "{} ({})".format(self.name, len(self.lines))
--- parser.py
+++ parser.py
@@ -1,8 +1,10 @@
-from bokeh.plotting import figure, show, output_file
-import collections
+from bokeh.plotting import figure, show, output_file, ColumnDataSource
 import os
 import re
 import sys
+import math
+sys.path.append(sys.path[0])
+from cast import Cast
 #import pandas as pd
 
 SCRIPTS_DIR = 'scripts'
@@ -14,38 +16,37 @@ default_regex = re.compile(r"\n[\t ]*([A-Z0-9'\- ]+[A-Z0-9'\-])(?: \(.+\))?\n(([
 def parse(filepath, regex=default_regex):
     with open(os.path.join(SCRIPTS_DIR, filepath)) as content_file:
         matches = re.finditer(regex, content_file.read())
-        return {(m.group(1), re.sub('\(.+\)', '', re.sub('\s+', ' ', m.group(2)).strip())) for m in matches}
+        return [(m.group(1), re.sub('\(.+\)', '', re.sub('\s+', ' ', m.group(2)).strip())) for m in matches]
+
+def to_cast(cl_table):
+    cast = Cast()
+    for cl in cl_table:
+        if cl[0] not in cast:
+            cast += cl[0]
+        cast.get_character(cl[0]).add_line(cl[1])
+    return cast
 
 filename = SCRIPT_NAME + '.txt'
 data = parse(filename)
+cast = to_cast(data)
 
-#print(data)
-chars = []
-lines = []
-words = []
-for d in data:
-    if d[0] not in chars:
-        chars.append(d[0])
-        lines.append([])
-        words.append([])
-    lines[chars.index(d[0])].append(d[1])
-    words[chars.index(d[0])].extend(re.findall(r"\b[A-Za-z]+(?:[-'][A-Za-z]+)?\b", d[1]))
-print(words[0])
-print(lines[0])
-#for d in data:
-#    print("{}:{}{}".format(d[0], " " * (16-len(d[0])), d[1][:84]))
-
-n_lines = collections.Counter([x[0] for x in data])
-n_lines = {c: n_lines[c] for c in n_lines if n_lines[c] >= 10}
-print(n_lines)
+source = ColumnDataSource(data=dict(
+    x=[c.average_wpL for c in cast],
+    y=[c.average_lpw for c in cast],
+    num_lines = [len(c.lines) for c in cast],
+    alpha = [len(c.lines) / max([len(x.lines) for x in cast]) for c in cast],
+    size = [16 * math.sqrt(len(c.lines) / max([len(x.lines) for x in cast])) for c in cast],
+    name=[c.name for c in cast]
+))
 
 p = figure(title="My Plot",
-           tools="hover")
+           tools="hover, pan, wheel_zoom, box_zoom, reset",
+           tooltips=[('name', '@name'), ('n_lines', '@num_lines'), ('avg_line_length', '@x'), ('avg_word_length', '@y')])
 
-p.xaxis.axis_label = "Average Line Length"
-p.yaxis.axis_label = "Average Word Length"
+p.xaxis.axis_label = "Average Line Length (words)"
+p.yaxis.axis_label = "Average Word Length (chars)"
 
-
+p.circle('x', 'y', fill_alpha='alpha', size='size', source=source)
 
 output_file(SCRIPT_NAME + '.html', title=SCRIPT_NAME)
 show(p)
